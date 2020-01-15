5e67aba1fee987d77fa144b0d16fbbd47be0d11d
Gzoref <gzoref@gmail.com>
2020-01-15 16:09:33 -0500

    Write a script or a program that votes 4096 times for your id here: http://158.69.76.135/level1.php

---
+++ level_0/0-level_0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import requests
+import time
+
+payload = {
+    'id': 1034,
+    'holdthedoor': 'Submit'
+}
+fails = 0
+
+for items in range(1024):
+    r = requests.post('http://158.69.76.135/level0.php', data=payload)
+    if r.status_code != 200:
+        print(f'Failed to post request number {items}!')
+        failed += 1
+
+print(f'Failed {fails} number of requests')
